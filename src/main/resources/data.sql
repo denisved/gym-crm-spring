57476504141cd450b6f1caf619d2a4e47d65b615
denisved <denysvedernikov333@gmail.com>
2026-05-14 21:03:47 +0300

    Data initialization for training_types

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO training_types (training_type_name) VALUES ('FITNESS') ON CONFLICT DO NOTHING;
+INSERT INTO training_types (training_type_name) VALUES ('YOGA') ON CONFLICT DO NOTHING;
+INSERT INTO training_types (training_type_name) VALUES ('ZUMBA') ON CONFLICT DO NOTHING;
+INSERT INTO training_types (training_type_name) VALUES ('STRETCHING') ON CONFLICT DO NOTHING;
+INSERT INTO training_types (training_type_name) VALUES ('CROSSFIT') ON CONFLICT DO NOTHING;
